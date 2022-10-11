08383c003f4c7506de41fa6df883333acbf70e8f
Ahmed Mgua <mguaahmed@gmail.com>
2022-10-11 20:06:18 +0200

    Add model type enum to define different model types

---
+++ BreakingBad/Models/ModelType.swift
@@ -0,0 +1,13 @@
+//
+//  ModelType.swift
+//  BreakingBad
+//
+//  Created by Ahmed Mgua on 11/10/2022.
+//
+
+import Foundation
+
+enum ModelType {
+    case characterWithImage,
+         characterWithoutImage
+}

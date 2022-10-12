f49fb0ebe64755154be1d399db6a416851cf62e0
Ahmed Mgua <mguaahmed@gmail.com>
2022-10-12 20:05:46 +0200

    Fetch characters in viewmodel

--- BreakingBad/ViewModel.swift
+++ BreakingBad/ViewModel.swift
@@ -5,14 +5,33 @@
 //  Created by Ahmed Mgua on 11/10/2022.
 //
 
+import Combine
 import UIKit
-
+enum CharacterServiceError {
+    case failedToFetchCharacters
+}
 final class ViewModel: NSObject {
-    private(set) var characters: [Model] = []
+    private(set) var characters = CurrentValueSubject<[Model], Never>([])
     private let charactersService: CharactersServiceProtocol
-    
+    private(set) var error = PassthroughSubject<CharacterServiceError, Never>()
+    private var cancellables = Set<AnyCancellable>()
     init(charactersService: CharactersServiceProtocol = CharactersService()) {
         self.charactersService = charactersService
+    }
+    
+    func fetchCharacters() {
+        charactersService.fetchCharacters()
+            .receive(on: RunLoop.main)
+            .catch { error -> AnyPublisher<[BBCharacter], Never> in
+                self.error.send(.failedToFetchCharacters)
+                return Just([])
+                    .setFailureType(to: Never.self)
+                    .eraseToAnyPublisher()
+            }
+            .sink { characters in
+                self.characters.send(characters)
+            }
+            .store(in: &cancellables)
     }
     
 }
@@ -20,22 +39,15 @@ final class ViewModel: NSObject {
 extension ViewModel: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        characters.value.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        if indexPath.row.isMultiple(of: 2) {
-            let cell = tableView.dequeueReusableCell(withIdentifier: ModelType.characterWithImage.rawValue, for: indexPath) as! CustomTableViewCell
-            
-            (cell as? ImageCharacterTableViewCell)?.configureUI()
-            return cell as UITableViewCell
-        } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: ModelType.characterWithoutImage.rawValue, for: indexPath) as! CustomTableViewCell
-            
-            (cell as? TextCharacterTableViewCell)?.configureUI()
-            return cell
-        }
+        let character = characters.value[indexPath.row]
+        let identifier = character.type.rawValue
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! CustomTableViewCell
+        cell.configure(with: character)
+        return cell
     }
     
     
